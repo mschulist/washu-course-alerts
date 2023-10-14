0f1caed4c5fa14f61f10332a92c64784e61dc76a
squaaawk <squaaawk@pop-os.localdomain>
2023-10-14 14:51:18 -0400

    merged

--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,10 @@
-import 'dotenv/config';
-import { getCourseInfo } from './getCourseInfo';
-import { sendText } from './sendText';
-
 // TODO: Add a cron job to run this every 5 minutes
 // Get data from firestore
 // For each course, check if there are open seats
 // If there are open seats, send a text to the user
 
+import { getSectionSeats } from "./parse";
+
+(async () => {
+    console.log(await getSectionSeats("FL2023", "L", "L24", "132", "F"));
+})();
