afbb0b5766dff51d88690bdce03ec183e30bef88
Mark Schulist <mschulist2@gmail.com>
2023-10-14 20:39:28 -0500

    changed to name

--- backend/index.ts
+++ backend/index.ts
@@ -27,9 +27,8 @@ async function main() {
             const seats = parsed[0];
             const seatsTaken = parsed[1];
             const seatsOpen = seats - seatsTaken;
-            console.log(user.email)
             if (seatsOpen > 0) {
-                sendText(user.phoneNumber, `${user.name}, there are ${seatsOpen} seats open in ${course.dept} ${course.crs} section ${course.sec}`);
+                sendText(user.phoneNumber, `${user.email}, there are ${seatsOpen} seats open in ${course.dept} ${course.crs} section ${course.sec}`);
                 removeCourse(user.email, course);
             }
         })
